d9d91ce8ddf1af0b4b2d409d49ffa3c3f1923848
RicardoCampanico7 <rickypias2005@gmail.com>
2026-02-11 12:07:16 +0000

    feat: estrutura inicial completa

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Post } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+
+@Controller("users")
+export class UsersController {
+  constructor(private prisma: PrismaService) {}
+
+  @Post()
+  create() {
+    return this.prisma.user.create({
+      data: {
+        email: `test${Date.now()}@mail.com`,
+      },
+    });
+  }
+
+  @Get()
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+}
