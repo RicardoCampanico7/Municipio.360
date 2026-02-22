dae7f02267b55d8239c8d860d5efb60badc80692
RicardoCampanico7 <rickypias2005@gmail.com>
2026-02-22 22:59:10 +0000

    feat(SCRUM-28): criar página de login no frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      
     </Routes>
   );
 }
