5b44e80ca9e0a93e14c97a5396cccf99c7bc965b
Guilherme Gaspar <gguerreirogaspar@gmail.com>
2026-02-23 11:46:27 +0000

    correção bug

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -11,6 +11,7 @@ export class AuthController {
   @HttpCode(HttpStatus.OK)
   login(@Body() dto: LoginDto) {
     return this.authService.login(dto);
+  }
   @Post('register')
   @HttpCode(HttpStatus.OK)
   register(@Body() dto: RegisterDto) {
