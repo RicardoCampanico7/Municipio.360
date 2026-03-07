7cc0a318f20ffd7416d38c99a1e267b22f6b1cd4
Ralan05 <martynyukalan@gmail.com>
2026-03-07 21:23:24 +0000

    feat:  Garantir estabilidade dos endpoints usados na sprint & Garantir estabilidade dos endpoints usados na sprint

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -26,11 +26,17 @@ export class AuthService {
 
     const user = await this.prisma.user.create({
       data: {
+        name: dto.name,
+        biNumber: dto.biNumber,
+        postalCode: dto.postalCode,
         email: dto.email,
         passwordHash,
       },
       select: {
         id: true,
+        name: true,
+        biNumber: true,
+        postalCode: true,
         email: true,
         role: true,
         certStatus: true,
@@ -44,6 +50,16 @@ export class AuthService {
   async login(dto: LoginDto) {
     const user = await this.prisma.user.findUnique({
       where: { email: dto.email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        role: true,
+        certStatus: true,
+        biNumber: true,
+        postalCode: true,
+        passwordHash: true,
+      },
     });
 
     if (!user) {
@@ -69,9 +85,12 @@ export class AuthService {
       tokenType: 'Bearer',
       user: {
         id: user.id,
+        name: user.name,
         email: user.email,
         role: user.role,
         certStatus: user.certStatus,
+        biNumber: user.biNumber,
+        postalCode: user.postalCode,
       },
     };
   }
